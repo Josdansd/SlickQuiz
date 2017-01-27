5503c92081032ea30b30419cc969ad373a47b4ef
Josdansd <jdsd@hotmail.es>
2017-01-27 12:53:04 -0600

    CSS Blocks for Buttons

--- js/slickQuiz.js
+++ js/slickQuiz.js
@@ -319,7 +319,7 @@
                             questionHTML.append('<a href="#" class="button ' + nextQuestionClass + ' ' + checkAnswerClass + '">' + nextText + '</a>');
                         } else {
                             questionHTML.append('<a href="#" class="button ' + nextQuestionClass + '">' + nextText + '</a>');
-                            questionHTML.append('<a href="#" class="button ' + checkAnswerClass + '">' + plugin.config.checkAnswerText + '</a>');
+                            questionHTML.append('<a href="#" class="button ' + checkAnswerClass + '" style="display: block;">' + plugin.config.checkAnswerText + '</a>');
                         }
 
                         // Append question & answers to quiz
@@ -488,8 +488,8 @@
                     }
                     questionLI.find('input').prop('disabled', true);
                     questionLI.find(_responses).show();
-                    questionLI.find(_nextQuestionBtn).fadeIn(300, kN(key,1));
-                    questionLI.find(_prevQuestionBtn).fadeIn(300, kN(key,2));
+                    questionLI.find(_nextQuestionBtn).fadeIn(300, kN(key,1)).css('display', 'block');
+                    questionLI.find(_prevQuestionBtn).fadeIn(300, kN(key,2)).css('display', 'block');
                     if (!questionLI.find(_prevQuestionBtn).length) kN(key,2).apply (null, []); // 2nd notch on key must be passed even if there's no "back" button
                 } else {
                     kN(key,1).apply (null, []); // 1st notch on key must be on both sides of if/else, otherwise key won't turn
